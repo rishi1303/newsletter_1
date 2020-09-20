5d3faa387c00d0595b6b1e39a7656f898943afeb
rishi1303 <61569495+rishi1303@users.noreply.github.com>
2020-09-20 19:56:22 +0530

    Update settings.py

--- email_sender/email_sender/settings.py
+++ email_sender/email_sender/settings.py
@@ -127,7 +127,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'rishabhmaheshwari80@gmail.com'
-EMAIL_HOST_PASSWORD = 'awesomemom'
+EMAIL_HOST_PASSWORD = '*********'
 
 LOGIN_REDIRECT_URL = 'sent'
 LOGOUT_REDIRECT_URL = 'base'
